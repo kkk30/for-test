d1238dcbf4d3f10770f3fae2c30bef991b9f417b
kkk30 <yarinige30@gmail.com>
2016-08-01 22:20:20 +0900

    マスター登録テスト

---
+++ constants/Constants.php
@@ -0,0 +1,19 @@
+<?php
+namespace SampleCode;
+
+class Constants
+{
+	//merchant credentials
+	const MERCHANT_LOGIN_ID = "5KP3u95bQpv";
+	const MERCHANT_TRANSACTION_KEY = "346HZ32z3fP4hTG2";
+	
+	const RESPONSE_OK = "1";
+	
+	//Recurring Billing
+	const SUBSCRIPTION_ID_GET = "2930242";
+
+	//Transaction Reporting
+	const TRANS_ID = "2238968786";
+}
+?>
+
